b0b0c85ee492d4821792cfb10f98b70cbd327d70
Sameer Naik <sameer@damagehead.com>
2015-06-28 19:43:43 +0530

    scripts: preserve environment in `sudo`

--- assets/install.sh
+++ assets/install.sh
@@ -4,7 +4,7 @@ set -e
 # add git user
 adduser --disabled-login --gecos 'GitLab CI Runner' ${GITLAB_CI_MULTI_RUNNER_USER}
 
-sudo -Hu ${GITLAB_CI_MULTI_RUNNER_USER} ln -s ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh ${GITLAB_CI_MULTI_RUNNER_HOME_DIR}/.ssh
+sudo -HEu ${GITLAB_CI_MULTI_RUNNER_USER} ln -s ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh ${GITLAB_CI_MULTI_RUNNER_HOME_DIR}/.ssh
 
 # download the gitlab-ci-multi-runner binary
 wget -O /usr/local/bin/gitlab-ci-multi-runner \
