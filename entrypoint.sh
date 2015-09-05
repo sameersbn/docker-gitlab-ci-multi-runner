53190bd1734addfc5706fa7119307c1a3b841d71
Sameer Naik <sameer@damagehead.com>
2015-09-05 22:53:51 +0530

    entrypoint.sh: use `[[` in conditional statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ create_data_dir() {
 generate_ssh_deploy_keys() {
   sudo -HEu ${GITLAB_CI_MULTI_RUNNER_USER} mkdir -p ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/
 
-  if [ ! -e ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa -o ! -e ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa.pub ]; then
+  if [[ ! -e ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa || ! -e ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa.pub ]]; then
     echo "Generating SSH deploy keys..."
     rm -rf ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa.pub
     sudo -HEu ${GITLAB_CI_MULTI_RUNNER_USER} ssh-keygen -t rsa -N "" -f ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa
@@ -23,7 +23,7 @@ generate_ssh_deploy_keys() {
 }
 
 update_ca_certificates() {
-  if [ -f "${CA_CERTIFICATES_PATH}" ]; then
+  if [[ -f ${CA_CERTIFICATES_PATH} ]]; then
     echo "Updating CA certificates..."
     cp "${CA_CERTIFICATES_PATH}" /usr/local/share/ca-certificates/ca.crt
     update-ca-certificates --fresh >/dev/null
@@ -34,7 +34,7 @@ appStart () {
   echo "Starting gitlab-ci-multi-runner..."
 
   # make sure the runner is configured
-  if [ ! -e ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/config.toml ]; then
+  if [[ ! -e ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/config.toml ]]; then
     appSetup
   fi
 
@@ -46,7 +46,7 @@ appStart () {
 }
 
 appSetup () {
-  if [ -n "${CI_SERVER_URL}" -a -n "${RUNNER_TOKEN}" -a -n "${RUNNER_DESCRIPTION}" -a -n "${RUNNER_EXECUTOR}" ]; then
+  if [[ -n ${CI_SERVER_URL} && -n ${RUNNER_TOKEN} && -n ${RUNNER_DESCRIPTION} && -n ${RUNNER_EXECUTOR} ]]; then
     sudo -HEu ${GITLAB_CI_MULTI_RUNNER_USER} \
       gitlab-ci-multi-runner register --config config.toml \
         -n -u "${CI_SERVER_URL}" -r "${RUNNER_TOKEN}" -d "${RUNNER_DESCRIPTION}" -e "${RUNNER_EXECUTOR}"
@@ -69,7 +69,7 @@ create_data_dir
 generate_ssh_deploy_keys
 update_ca_certificates
 
-case "$1" in
+case ${1} in
   app:start)
     appStart
     ;;
@@ -80,11 +80,11 @@ case "$1" in
     appHelp
     ;;
   *)
-    if [ -x ${1} ]; then
+    if [[ -x ${1} ]]; then
       $@
     else
       prog=$(which ${1})
-      if [ -n "${prog}" ] ; then
+      if [[ -n ${prog} ]] ; then
         shift 1
         exec ${prog} $@
       else
