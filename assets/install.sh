cb9d8e31b88bcf8ee5e3f7add582d8d0bd8be6f5
Anton Fedotov <fedotovad@ecotelecom.ru>
2015-12-14 10:33:40 +0300

    release 0.7.2

--- assets/install.sh
+++ assets/install.sh
@@ -8,4 +8,4 @@ sudo -HEu ${GITLAB_CI_MULTI_RUNNER_USER} ln -s ${GITLAB_CI_MULTI_RUNNER_DATA_DIR
 
 # download the gitlab-ci-multi-runner binary
 curl -s https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh | sudo bash
-sudo apt-get install gitlab-ci-multi-runner=0.6.2
+sudo apt-get install gitlab-ci-multi-runner=0.7.2
