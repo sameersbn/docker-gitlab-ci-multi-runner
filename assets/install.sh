6d3cf9cae7004f05b0e663f6546f498ce69c14e6
Sameer Naik <sameer@damagehead.com>
2015-12-24 21:32:03 +0530

    install `gitlab-ci-multi-runner` deb from the Dockerfile
    Removed the use of `script.deb.sh` to download and install
    `gitlab-ci-multi-runner` debian package.

--- assets/install.sh
+++ assets/install.sh
@@ -5,8 +5,3 @@ set -e
 adduser --disabled-login --gecos 'GitLab CI Runner' ${GITLAB_CI_MULTI_RUNNER_USER}
 
 sudo -HEu ${GITLAB_CI_MULTI_RUNNER_USER} ln -s ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh ${GITLAB_CI_MULTI_RUNNER_HOME_DIR}/.ssh
-
-# download the gitlab-ci-multi-runner binary
-curl -s https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh | sudo bash
-sudo apt-get install gitlab-ci-multi-runner=${GITLAB_CI_MULTI_RUNNER_VERSION}
-rm -rf /var/lib/apt/lists/*
