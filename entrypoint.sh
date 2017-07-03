d050d92c6602e2fbddb903a3abdd540c3456d1ea
digitalLumberjack <digitallumberjack@gmail.com>
2017-07-03 06:50:49 +0200

    fixed spelling on concurrent parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -77,7 +77,7 @@ configure_ci_runner() {
         gitlab-ci-multi-runner register --config ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/config.toml
     fi
     if [[ -n ${RUNNER_CONCURRENT} ]];then
-      sed -i "s/concurrent = .*/concurent = ${RUNNER_CONCURRENT}/" ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/config.toml
+      sed -i "s/concurrent = .*/concurrent = ${RUNNER_CONCURRENT}/" ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/config.toml
     fi
   fi
 }
